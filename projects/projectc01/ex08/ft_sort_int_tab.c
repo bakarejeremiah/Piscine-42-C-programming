685ef3bd6002f0c85cd884049286a726c6db4e30
bakarejeremiah <bakarejeremiah@gmail.com>
2022-05-12 21:48:15 +0300

    done

---
+++ projects/projectc01/ex08/ft_sort_int_tab.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_sort_int_tab.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: coder <coder@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/02/21 23:03:17 by coder             #+#    #+#             */
+/*   Updated: 2022/02/23 19:22:57 by coder            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void	ft_print_tab(int *tab, int size)
+{
+	int		i;
+
+	i = 0;
+	while (i < size)
+	{
+		ft_putchar(tab[i] + '0');
+		i++;
+	}
+}
+
+void	ft_sort_int_tab(int *tab, int size)
+{
+	int		i;
+	int		j;
+	int		tmp;
+
+	j = 0;
+	while (j < size)
+	{
+		i = 0;
+		while (i < size)
+		{
+			if (tab[i] >= tab[j])
+			{
+				tmp = tab[i];
+				tab[i] = tab[j];
+				tab[j] = tmp;
+			}
+			i++;
+		}
+		j++;
+	}
+}
